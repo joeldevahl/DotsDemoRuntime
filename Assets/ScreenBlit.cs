d7fad006498c090312b13fca0d414f480cb9bd0d
Joel de Vahl <joel@devahl.com>
2019-10-14 12:34:10 +0200

    Initial import

---
+++ Assets/ScreenBlit.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Unity.Mathematics;
+
+//[ExecuteInEditMode]
+public class ScreenBlit : MonoBehaviour
+{
+    private CommandBuffer cmd;
+    private int width;
+    private int height;
+    private ComputeBuffer computeBuffer;
+    private NativeArray<float4> buffer;
+    public Material blitMaterial;
+    
+    void OnEnable()
+    {
+        cmd = new CommandBuffer();
+        Camera.main.AddCommandBuffer(CameraEvent.AfterEverything, cmd);
+        height = Camera.main.pixelHeight;
+        width = Camera.main.pixelWidth;
+        buffer = new NativeArray<float4>(width * height, Allocator.Persistent);
+        computeBuffer = new ComputeBuffer(buffer.Length * 16, 16, ComputeBufferType.Structured);
+        
+        // fill initial data
+        for (int i = 0; i < buffer.Length; ++i)
+        {
+            buffer[i] = new float4(1.0f, 0.0f, 1.0f, 1.0f);
+        }
+        computeBuffer.SetData(buffer);
+        
+        // set up blitting command buffer
+        blitMaterial.SetBuffer("_buffer", computeBuffer);
+        cmd.DrawProcedural(Matrix4x4.identity, blitMaterial, 0, MeshTopology.Triangles, 3, 1);
+    }
+
+    private void OnDisable()
+    {
+        Camera.main.RemoveCommandBuffer(CameraEvent.AfterEverything, cmd);
+        cmd.Dispose();
+        computeBuffer.Dispose();
+        buffer.Dispose();
+    }
+
+    private void Update()
+    {
+        // fill per frame data
+        for (int i = 0; i < buffer.Length; ++i)
+        {
+            buffer[i] = new float4(1.0f, 0.0f, 1.0f, 1.0f);
+        }
+        computeBuffer.SetData(buffer);
+    }
+}
---
+++ Assets/ScreenBlit.shader
@@ -0,0 +1,53 @@
+﻿Shader "ScreenBlit"
+{
+    Properties
+    {
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            ZWrite Off
+            ZTest Always
+            Blend Off
+            Cull Off
+            
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+            
+            StructuredBuffer<float4> _buffer;
+
+            struct appdata
+            {
+                uint vertexID : SV_VertexID;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                float2 uv = float2((v.vertexID << 1) & 2, v.vertexID & 2);
+                o.vertex = float4(uv * 2.0 - 1.0, UNITY_NEAR_CLIP_VALUE, 1.0);
+                return o;
+            }
+
+            float4 frag (v2f i) : SV_Target
+            {
+                uint index = uint(i.vertex.y) * uint(_ScreenParams.x) + uint(i.vertex.x);
+                return _buffer[0];
+            }
+            ENDHLSL
+        }
+    }
+}
+
